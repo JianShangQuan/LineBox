66b4df6905643ada98ee8cb737fa12024a30aa52
简尚全 <jianshangquan3@gmail.com>
2022-05-05 21:11:07 +0630

    apply firebase hosting

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,30 @@
 const { initializeApp } = require('firebase/app');
+const { getAnalytics } = require('firebase/analytics');
 const { getDatabase, ref, set, get, remove, onValue, off} = require('firebase/database');
 
 // console.log(firebase);
 
 const config = {
-    apiKey: "BJKemhuwY7UeGnLTlzPEyRzHOYpfHFTZstGH4XJmEWxexH7zlivlXO3vZeigxNs0RHkRkqcqGKyzDT_Ipk4YMPU",
+    apiKey: "AIzaSyCcoQBLBxLEETyvF0b8yPXudAxaoe7nhR8",
     authDomain: "linebox-3725d.firebaseapp.com",
-    databaseURL: "https://linebox-3725d-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    storageBucket: "linebox-3725d.appspot.com"
+    databaseURL: "https://linebox-3725d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "linebox-3725d",
+    storageBucket: "linebox-3725d.appspot.com",
+    messagingSenderId: "307404446225",
+    appId: "1:307404446225:web:1f95fbcd2029e5ca89afb0",
+    measurementId: "G-7DXJW7LSM2"
 }
 
 
 const app = initializeApp(config);
+const db = getDatabase(app);
+const analytics = getAnalytics(app);
 
 console.log('DB initialized');
 
 
 module.exports = {
-    db: getDatabase(app),
+    db, analytics,
     app, ref, set, get, remove,
     off,
     onValue
